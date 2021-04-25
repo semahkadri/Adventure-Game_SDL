e9dd5e3e53f40593b6e560dcc8bdafb000d7972c
semahkadri <semahkadri@esprit.tn>
2021-04-25 20:03:34 +0100

    intégration jeu

---
+++ option.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h> 
+#include <SDL/SDL_ttf.h>
+
+void option(SDL_Surface* ecran,Mix_Chunk *effet,Mix_Chunk *effet2,Mix_Music *musique,int *mute,int *continuer,SDL_Rect *posvolb);
